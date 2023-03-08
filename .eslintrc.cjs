d7b1476e2231dc7a331437d0eca1c47461704aa4
Julien Zapata Duque <jzapata@live.nl>
2023-03-08 19:03:02 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['svelte3', '@typescript-eslint', 'prettier'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    'prettier/prettier': ['error'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'func-style': [
+      'error',
+      'expression',
+      {
+        allowArrowFunctions: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          arguments: false,
+          attributes: false,
+        },
+      },
+    ],
+    'spaced-comment': 'error',
+  },
+};
